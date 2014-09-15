ba7fd7d1569dae16985848549c41b2f2cecb6164
unknown <DmitryMyadzelets@gmail.com>
2014-09-15 15:40:48 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,367 @@
+﻿<!DOCTYPE HTML>
+<html lang="it">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width">
+	<meta name="description" content="Corso di Lingua Russa a Bologna">
+	<meta name="keywords" content="corso di russo a bologna,
+	corso russo,
+	scuola di lingue,
+	corso di lingua russa,
+	lezioni di russo a Bologna,
+	bologna centro,
+	portico delle parole,
+	porticodelleparole,
+	porticodelleparole.it,
+	offerta,
+	prezzi bassi,
+	sconti,
+	portici,
+	corsi di PACCHETTI PROMOZIONALI a Bologna,
+	corsi di lingue a Bologna,
+	corsi di inglese a Bologna,
+	corsi di russo a Bologna,
+	corsi di Italiano per stranieri a Bologna,
+	ripetizioni di russo a Bologna,
+	viaggi a Bologna,
+	giochi a Bologna,
+	test a Bologna,
+	ripetizioni di Russo a Bologna,
+	preparazione agli esami di maturità a Bologna,
+	preparazione esami universitari a Bologna,
+	">
+
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<title>
+		Corso di Russo a Bologna - Associazione Portico delle Parole
+	</title>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+		_gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+		_gaq.push(['_setAccount', 'UA-44617461-1']);
+		_gaq.push(['_trackPageview']);
+		(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+
+</head>
+
+<body>
+
+<div style="background: white; padding: 0 1em" class="shadow">
+
+
+<div >
+<!-- <img src="./img/bologna-moscow.gif" alt="Corso di lingua Russa"> -->
+		<img src="./img/bologna-moscow-rainbow-portico.gif" 
+		alt="Portico delle parole. Associazione. Corso di lingua Russa"/>
+</div>
+
+<div class="center">
+<p>Vuoi imparare <b>il russo parlato</b> e scritto in maniera coinvolgente, divertente e sempre motivante? Ti interessa saper parlare russo senza fare noiosissimi esercizi di grammatica? Vieni da noi e fai il corso di russo<br> <b>Immerssione in Russia</b>!</p>
+
+<p style="color:red"><b>Il corso di russo per i principianti in OFFERTA si comincia dal 22 di Settembre.
+<br>Mancano solo pochi giorni ed è rimasto soltanto 1 posto!</b>
+</p>
+
+<p>
+	<?php
+		$start_date = strtotime("7-10-2014");
+		$now = strtotime("now");
+		$days_left = floor(($start_date - $now)/(60*60*24));
+		if ($days_left > 0) {
+			echo "<b>La prossima edizione del corso di russo per i principianti si commincia dal 7 di Ottobre. </b>";
+			echo "<b>Mancano solo ", $days_left, " giorni alla prima lezione!</b>";
+		}
+	?>
+</p>
+
+</div>	
+
+	
+<div align="right" style="font-size: 1.5em; padding: 0.2em;">
+		<img src="./img/phone-icon.png" alt="Phone" 
+		style="height:1em; width:1em; vertical-align: middle;"/>
+		<span style="vertical-align: middle;">&nbsp;
+			327 66 17 027</span>
+</div>
+
+<p>
+	Ciao! Piacere, siamo la associazione culturale <b>Portico delle Parole</b> e siamo aperti a chi voglia imparare il russo parlato e scritto, speriamo che tu lo sia uno di questi! Per conoscere meglio guarda il video sottо. 
+</p>
+
+
+<!-- Video -->
+<iframe id="ytplayer" type="text/html" width="640" height="480"
+  src="http://www.youtube.com/embed/72R4ZLbuNPg?rel=0&controls=0&showinfo=0&origin=http://www.porticodelleparole.it/"
+  frameborder="0"/></iframe>
+
+<!-- <div class="center" style="position: relative; top: -1em;">
+	<div id="association">Associazione</div>
+	<div id="portico">Portico delle parole</div>
+</div>
+ -->
+
+<p>
+	A seguito ci sono delle domande che tu come nostro ipotetico futuro studente potresti farci e noi ti diamo le risposte:
+</p>
+<h2>
+	I nostri punti di forza
+</h2>
+<ul>
+	<li>La nostra idea è <b>promuovere la lingua e cultura russa in Italia</b> e farle conoscere a più persone possibili. Siamo speccializzati solo in russo e facciamo solo corsi di russo.</li>
+	<li><b>I nostri prezzi sono ragionevoli</b> e affrontabili praticamente per tutti, non abbiamo fini di lucro.</li>
+	<li>I nostri insegnanti sono tutti madrelingua russa laureati nell'insegnamento della lingua a stranieri. Forniscono basi molto solide della <b>lingua parlata e scritta</b> ed è proprio quello di cui hai bisogno.</li>
+	<li>Insegniamo il russo parlato utilizzando giochi, dialoghi, situazioni divertenti e coinvolgenti che rendono il processo di studio molto piacevole.</li>
+	<li>Diamo la possibilità di conoscere i russi che abitano a Bologna organizzando aperetivi ed eventi culturali. Per chi è interessato cerchiamo un "language partner" russo che studia italiano e abita a Bologna.</li>
+</ul>
+
+
+<h2>
+	Qual’è’ il programma del corso? Che cosa comprende? 
+</h2>
+<p>
+	Nel corso del programma di 20 ore affronti le seguenti tematiche in 7 passi:
+</p>
+<ol>
+	<li>L'alfabeto cirillico.</li>
+	<li>La fonetica.</li>
+	<li>Conoscenza e uso delle principali strutture e frasi della lingua russa: sostantivi ed aggettivi al singolare e al plurale, pronomi, verbi (coniugazione al presente e al passato) e caso prepositivo.</li>
+	<li>Conoscenza dei gruppi di parole, delle espressioni più comuni e loro pratica intensiva.</li>
+	<li>Le frasi più utilizzate per l’uso quotidiano e per il lavoro.</li>
+	<li>Conoscenza della cultura russa, tramite testi semplici in russo e video.</li>
+	<li>Conversazione: presentazioni, come dare informazioni, come parlare di te stesso e della tua famiglia; inoltre formulare frasi per sostenere una conversazione di uso quotidiano e affrontare tematiche e situazioni del mondo del lavoro.</li>
+</ol>
+<p>Inoltre alle lezioni i tuoi insegnanti utilizzano:</p> 
+<ul>
+	<li>Immagini</li>
+	<li>Canzoni</li>
+	<li>Testi originali, dialoghi di esempio</li>
+	<li>Materiale audio e video</li>
+	<li>Giochi</li>
+</ul>
+<p>Le nostre tre specialità che non troverai da nessuna parte sono (e <b>tutto è già INCLUSO nel prezzo)</b>:</p>
+<ul>
+	<li>Dopo ogni lezione recevirai email con link per vedere video, ascoltare dialoghi, canzoni e materiali per fare i compiti.</li>
+	<li>Alla fine del corso organizziamo una cena con russi madrelingua, dove potrai praticare il tuo russo.</li>
+	<li>I libri (già stampati) per il corso te li forniamo noi.</li>
+</ul>
+
+
+
+<h2>
+	Chi sono i vostri insegnanti?
+</h2>
+<p>
+	Ti faranno lezione insegnanti madre lingua russa (<b>sono proprio russi</b>), tutti laureati nell`insegnamento delle lingue. Hanno esperienza di più di tre anni di insegnamento del russo agli italiani. Per te questo fatto garantisce un alto livello di insegnamento e di conseguenza buoni e veloci risultati.
+</p>
+
+
+
+<h2>
+	Alla gente di quale età è indirizzato il corso?
+</h2>
+<p>
+	Il corso "Immerssione in Russia" è fattibile a qualsiasi persona di qualsiasi età perchè i contenuti del corso sono spiegati e rapresentati cosi` chiaramente che chiunchue lo faccia possa farlo con massimo successo. 
+	Quindi il corso va` benissimo per te!
+</p>
+
+
+
+<h2>
+	Quante volte a settimana ci sono le lezioni?
+</h2>
+<p>
+	Hai lezioni una volta alla settimana il giorno e l`ora che preferisci!
+</p>
+
+
+
+<h2>
+	Quanto dura una lezione?
+</h2>
+<p>
+	Una lezione dura due ore con una pausa di cinque minuti.
+</p>
+
+
+
+<h2>
+	C'è qualcuno chi ha già fatto questo corso?
+</h2>
+<p>
+	Si, certo! Abbiamo molti nostri clienti che hanno già fatto il corso "Immerssione in Russia", puoi leggere i loro giudizi:
+</p>
+
+
+<!-- Reviews -->
+<iframe id="reviews" type="text/html" width="100%" height="400"
+  src="./reviews.html"
+  frameborder="0"/></iframe>
+
+
+
+<h2>
+	Si ma quanto costa il corso?
+</h2>
+<p>
+	Essendo senza fini di lucro i prezzi come non si trovano in nessuna scuola di lingue a Bologna.<br>
+	Ecco il listino dei nostri <b>prezzi eccezionali</b>. 
+</p>
+<table class="gridtable">
+	<tr>
+		<th>Individuale </th>
+		<th>In due</th>
+		<th>In gruppo <br>min 5 - max 10 persone</th>
+	</tr>
+	<tr>
+		<td>1 ora (60 min) – 27&#8364;<br>
+			<span style="color:red">
+			<b>10 ore* – <span style="text-decoration: line-through;">217&#8364;</span> 
+			adesso in promozione 187&#8364;</b>
+			</span>
+			<br>20 ore* – 417&#8364;
+			</td>
+		<td>1 ora (60 min) – 34&#8364;<br>10 ore* – 297&#8364;<br>20 ore* – 497&#8364;
+			</td>
+		<td>
+			<span style="color:red"><b>
+			20 ore – 
+			<span style="text-decoration: line-through;">207&#8364;</span> 
+			adesso in promozione limitata 
+			<span style="font-size:large">147&#8364;</span></b>
+			</span>
+		</td>
+	</tr>
+</table>
+<p>
+	*Ti diamo flessibilità, in caso tu abbia impegni potrai spostare tre lezioni su dieci. Importante e'pero'avvisarci almeno tre ore prima della lezione.
+	<br>
+	Siccome siamo una associazione culturale c’è obbligo della tessera associativa che costa solo 10&#8364;.
+</p>
+
+
+<h2>
+	Dove si trova la sede della vostra associazione?
+</h2>
+<p>
+	Ci troviamo nel vero e proprio cuore di Bologna, nel centro storico, in via Nosadella, 15 B. 
+</p>	
+<a href="http://maps.google.com/maps?q=Via+Nosadella+15B,+Bologna,+Italy&amp;hl=en&amp;sll=44.49522,11.352654&amp;sspn=0.053693,0.111494&amp;hnear=Via+Nosadella,+15B,+Bologna,+Emilia-Romagna,+Italy" target="_blank">
+<img src="./img/map.gif" alt="Map: via Nosadella, 15B, Bologna">
+</a>
+<p>
+	Posto che puoi raggiungere molto facilmente da tutte le parti della città, c’è anche il parcheggio publico nella piazetta di Porta Saragozza che si trova a 5-7 min camminando.
+</p>
+
+
+
+<!-- <h2>
+	Va bene, quando posso iniziare il corso "Immerssione in Russia"?
+</h2>
+<p>
+	Il corso inizia <b>il 08 di Settembre 2014</b> in giorno che ci sarà concordato con tutti i partecipanti del gruppo.
+	<span style="color:red">
+	<?php
+		$start_date = strtotime("01-09-2014");
+		$now = strtotime("now");
+		$days_left = floor(($start_date - $now)/(60*60*24));
+		if ($days_left > 0) {
+			echo "<b>Mancano solo ", $days_left, " giorni alla prima lezione!</b>";
+		}
+	?>
+	</span>
+</p>
+ -->
+
+
+<h2>
+	Quando e come posso pagare?
+</h2>	
+<p>
+	Se sei interessato ad immergerti nella lingua e cultura russa chiama il numero: 
+	<b>327 66 17 027</b> oppure scrivi una e-mail: <a href="mailto:info@porticodelleparole.it">info@porticodelleparole.it</a>, ti contatterà uno dei nostri assistenti. 
+</p>
+
+
+
+<p>
+Ma per garantirti subito il tuo posto nel nuovo gruppo fai l'iscrizione on-line compilando questo modulo:
+<script type="text/javascript">
+<!--
+function autoResize(id){
+    var newheight;
+    var newwidth;
+    if(document.getElementById){
+        newheight=document.getElementById(id).contentWindow.document.body.scrollHeight;
+    	document.getElementById(id).height= (newheight + 35) + "px";  /*35 is a magic number :) */
+    }
+}
+//-->	
+</script>
+
+
+<div>
+	<iframe id="iscrizione" src="./iscrizione.php" 
+		onLoad="autoResize('iscrizione');"
+		scrolling="no"></iframe>
+</div>	
+
+Dopo fissiamo appuntamento quando tu vieni in nostra sede prima dell’inizio del corso per versare la tua quota di partecipazione al corso. 
+Ti communichiamo il giorno e l’ora esatta quando devi venire alla prima lezione.
+</p>
+
+
+
+
+<h2>
+	Alla fine
+</h2>	
+<p>
+	<b>Ti vogliamo ringraziare per aver dedicato il tuo tempo a conoscere la nostra associazione. Noi siamo sicuri che il nostro corso sarà estremamente utile e interessante per te se vuoi immerggerti nel mondo della lingua e cultura russa!</b>
+</p>
+
+<!-- <p>
+	Ti ricordiamo che l’iscrizione al corso è già aperta e durerà fino il 4 del Marzo 2014. Non sappiamo esattamente quando rifacciamo questo corso però i prezzi non saranno già mai cosi’ bassi come adesso.
+</p>
+ -->
+<p>
+	Se vieni con un amico/a avete uno sconto di 10&#8364; ciascuno – la tessera associativa ti regaliamo noi!
+</p>
+
+<p>
+	<em>I tuoi insegnanti Olga (Irkutsk, Russia, 26 anni) e Daria (Mosca, Russia, 28 anni)
+	</em>
+	<br>&nbsp;
+</p>
+
+
+<h2>
+	Foto
+</h2>
+<object width="640" height="480">
+	<param name="flashvars" value="offsite=true&lang=en-us&page_show_url=%2Fphotos%2F126701579%40N07%2Fsets%2F72157647133686116%2Fshow%2F&page_show_back_url=%2Fphotos%2F126701579%40N07%2Fsets%2F72157647133686116%2F&set_id=72157647133686116&jump_to="></param>
+	<param name="movie" value="https://www.flickr.com/apps/slideshow/show.swf?v=1811922554"></param>
+	<param name="allowFullScreen" value="true"></param>
+	<embed type="application/x-shockwave-flash" src="https://www.flickr.com/apps/slideshow/show.swf?v=1811922554" allowFullScreen="true" flashvars="offsite=true&lang=en-us&page_show_url=%2Fphotos%2F126701579%40N07%2Fsets%2F72157647133686116%2Fshow%2F&page_show_back_url=%2Fphotos%2F126701579%40N07%2Fsets%2F72157647133686116%2F&set_id=72157647133686116&jump_to=" width="640" height="480"></embed>
+</object>
+
+</div>
+
+<div style="font-size: 0.8em" class="center">
+	<address>
+	Associazione Portico delle Parole<br>
+	via Nosadella 15B, 40123 Bologna, Italia <br>
+	Tel: +39 327 6617027<br>
+	<a href="mailto:info@porticodelleparole.it">info@porticodelleparole.it</a>&nbsp;&nbsp;&nbsp;
+	<a href="http://www.porticodelleparole.it">www.porticodelleparole.it</a>
+	</address>
+	C.F. 91361610370
+</div>
+
+</body>
+</html>
---
+++ iscrizione.php
@@ -0,0 +1,152 @@
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+</head>
+<body>
+
+
+<?php
+	class State {
+		const typing = 0;
+		const fixing = 1;
+		const done = 2;
+	}
+
+	$state = State::typing;
+
+	if ($_POST) {
+		$state = State::fixing;
+
+	   	$valid_name = !empty($_POST['name']);
+	   	$valid_email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+	   	if ($valid_name and $valid_email) {
+	   		$state = State::done;
+	   	}
+	}
+
+	$values = array();
+
+
+	if ($_POST) {
+		/* Debug
+	    echo '<pre>';
+	    echo htmlspecialchars(print_r($_POST, true));
+	    echo '</pre>';
+	    */
+	   
+	   	do {
+
+	   		if ($state != State::done) { break; }
+
+			/* Make file name. It is current date + CSV */
+			$str_date = date('Ymd', strtotime('now'));
+			$fname = './log/'.$str_date.'.csv';
+
+			$fsize = 0;
+			
+			if (file_exists($fname)) { $fsize = filesize($fname); }
+
+
+			if ($fp = fopen($fname, 'a')) {
+				/* Write CSV header if file is new or it's empty.
+				Newly created file has size 1.
+				*/
+			
+				if ($fsize < 1) {
+					$names = array_merge(array('ip'), array_keys($_POST));
+					fputcsv($fp, 
+						$names,
+						';');
+				}
+
+				/* Maximum file size (protection from overfill) */
+				$fsize_max = 100*1024;
+
+				if ($fsize < $fsize_max) {
+					$values = array();
+					$values['ip'] = getenv("REMOTE_ADDR");
+					$values = array_merge($values, $_POST);
+					fputcsv($fp, 
+						$values,
+						';');
+				}
+
+				fclose($fp);
+			}
+
+	   	} while (false);
+	}
+?>
+
+<div <?php if ($_POST) { echo '___style="visibility:hidden; height:0"'; } ?>>
+
+	<form method="POST" action="<?php $_PHP_SELF ?>">
+
+	<?php 
+		$err_msg = "<span style='color: red'> non e digitato correttamente </span>"; 
+	?>
+
+
+	<p>Nome, Cognome <?php if (($state == State::fixing) and !$valid_name) { echo $err_msg; } ?>
+		<br/>
+		<input type="text" name="name" style="width:100%" value="<?php echo $_POST['name']?>">
+	</p>
+
+	<p>Età (opzionale)<br/>
+		<input type="text" name="age" size="7em" value="<?php echo $_POST['age']?>">
+	</p>
+
+	<p>Giorni e ora preferiti per frequentare lezioni (scegliere più varianti possibili)<br/>
+		<input type="text" name="schedule" style="width:100%" value="<?php echo $_POST['schedule']?>">
+	</p>
+
+	<p>Indirizzo e-mail <?php if (($state == State::fixing) and !$valid_email) { echo $err_msg; } ?>
+		<br/>
+		<input type="text" name="email" style="width:100%" value="<?php echo $_POST['email']?>">
+	</p>
+
+	<p>Numero di telefono (opzionale)<br/>
+		<input type="text" name="phone" size="15em" value="<?php echo $_POST['phone']?>">
+	</p>
+
+
+	<p>
+		Se già studiato il russo, per quanto tempo?<br/>
+		 <input type="text" name="time_learned" style="width:100%" value="<?php echo $_POST['time_learned']?>">
+	</p>
+
+	<input type="submit" value="Invia" style="height:2em; width:5em"> <?php  if ($state == State::done) {echo "Grazie, ", $_POST['name'];} ?>
+
+</div>
+
+<div <?php if (!$_POST) { echo 'style="visibility:hidden; height:0"'; } ?>>
+</div
+
+
+
+<?php
+
+	if ($state == State::done) {
+
+		$to  = 'info@porticodelleparole.it'.',';
+		$to .= 'DmitryMyadzelets@gmail.com';
+		$subject = 'Iscrizione';
+
+		$message = '';
+		foreach ($values as $key => $value) {
+            $message = $message.$key.' : '.$value."\r\n";
+        }
+
+		$headers   = array();
+		$headers[] = "MIME-Version: 1.0";
+		$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+		$headers[] = "From: Portico delle Parole <info@porticodelleparole.it>";
+		$headers[] = "Reply-To: Recipient Name <receiver@domain3.com>";
+		$headers[] = "Subject: {$subject}";
+		$headers[] = "X-Mailer: PHP/".phpversion();
+
+		echo mail($to, $subject, $message, implode("\r\n", $headers));
+	}
+?>
+
+</body>
