6099bda40d0649fcc10cfae93d56f56005e97193
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-12-17 16:38:38 +0100

    Added new events

--- te-di-sabato/index.php
+++ te-di-sabato/index.php
@@ -91,7 +91,7 @@
         <li>
             <b>Maslenitsa</b> &mdash; il carnevale russo: come si festeggiava la Maslenitsa prima e come si
             festeggia adesso, Maslenitsa in letteratura, folklore, arte, canzoni ecc, tante curiosità su
-            questa festa e tanti bliny (le crepe’).
+            questa festa e tanti bliny (le crepes).
             <span class='authors'>Daria Kanishcheva di Pskov e Evgenia Litvin di Mosca</span>
         </li>
         <li>
@@ -104,6 +104,9 @@
             l’Assedio di Leningrado, tanti fatti interessanti sulla guerra che non sono conosciute nel
             mondo occidentale, canzoni patriottiche.
             <span class='authors'>Liubov Dyshlyuk di Volgograd</span>
+        </li>
+        <li>
+            Festa del <b>Capodanno</b> russo con le insalate e le bevande tipiche.
         </li>
     </ul>
 
@@ -141,6 +144,22 @@
             <b>Metropolitana di Mosca</b>: storia, curiosità, le fermate più belle.
             <span class='authors'>Ekaterina Ponamareva di Mosca</span>
         </li>
+        <li>
+            <b>Popoli della Russia</b>, gente Buriati &mdash; буряты: cultura buriata, tradizioni e costumi.
+            <span class='authors'>Liubov Ermolaeva di Irkutsk</span>
+        </li>
+        <li>
+            <b>Novosibirsk</b> è la capitale della Siberia.
+            <span class='authors'>Anna Shkapa di Novosibirsk</span>
+        </li>
+        <li>
+            <b>Città dell'anello d'oro</b>: fatti interessanti, storia, posti da visitare, 
+            consigli per il viaggio.
+            <span class='authors'>Anna Surikova di Mosca</span>
+        </li>
+        <li>
+            <b>La città di Gorodec</b>: particolarità storiche e culturali della città e della regione. <span class='authors'>Valeria Belova di Gorodec</span>
+        </li>
     </ul>
 
     <h3>Cultura</h3>
@@ -151,7 +170,7 @@
             <span class='authors'>Liubov Dyshlyuk di Volgograd</span>
         </li>
         <li>
-            <b>Vladimir Vysotsky</b> - uno degli uomini più amati nell`Unione Sovietica degli anni `60-`70. 
+            <b>Vladimir Vysotsky</b> &mdash; uno degli uomini più amati nell`Unione Sovietica degli anni `60-`70. 
             L'attore del cinema e del teatro, il cantautore dalla voce magica ha espresso le idee
             della intera generazione.
             <span class='authors'>Evgenia Litvin di Mosca</span>
@@ -177,6 +196,14 @@
         <li>
             <b>Pittori russi</b> e le loro opere nei musei di Mosca e San Pietroburgo.
             <span class='authors'>Liubov Dyshlyuk di Volgograd</span>
+        </li>
+        <li>
+            La vita e la poesia di <b>Vladimir Mayakovsky</b>.
+            <span class='authors'>Valeria Belova di Gorodec</span>
+        </li>
+        <li>
+            <b>L’alfabetizzazione</b> e l’educazione della Rus’ antica.
+            <span class='authors'>Anna Shkapa di Novosibirsk</span>
         </li>
     </ul>
 
