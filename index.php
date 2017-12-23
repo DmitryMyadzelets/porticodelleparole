d6abb47911a7f1598dd2436a57b3138e4f71dba0
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-12-23 19:38:26 +0100

    Added price change on 1st Jan 2018

--- index.php
+++ index.php
@@ -575,10 +575,23 @@
             <h3>In gruppo</h3>
         </div>
         <p>
-            20 ore – <span style="text-decoration: line-through;">240&#8364;</span> <span style="color:red">adesso in offerta 197&#8364;</span>  (9,8 all'ora)
+<?php
+
+    $date = strtotime('2018-01-01');
+    $now = strtotime("now");
+    $days_left = 1 + floor(($date - $now)/(60*60*24));
+
+    if ($days_left > 0) {
+        echo "20 ore – <span style=\"text-decoration: line-through;\">240&#8364;</span>";
+        echo " <span style=\"color:red\"> adesso fino al 31 dicembre 197&#8364;</span> (9,8 all'ora)";
+    } else {
+        echo "20 ore – <span style=\"text-decoration: line-through;\">260&#8364;</span>";
+        echo " <span style=\"color:red\"> adesso in offerta 227&#8364;</span> (11,35 all'ora)";
+    }
+?>
         </p>
         <p>
-            I gruppi sono formati da 5 &mdash; max. 10 persone</span>
+            I gruppi sono formati da 4 &mdash; max. 10 persone</span>
         </p>
     </div>
 </div>
