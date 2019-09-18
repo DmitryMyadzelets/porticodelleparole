35d17c2783618fc68e2543d38e50c345e5a55a6a
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2019-09-18 13:20:03 +0200

    Add pravicy page

---
+++ privacy/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Cache-control" content="private">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="description" content="Vuoi imparare il russo parlato e scritto in maniera coinvolgente, divertente e sempre motivante? Vieni da noi e fai il Corso di Lingua Russa a Bologna in Associazione Portoco delle Parole"> -->
+    <meta name="description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, 
+    offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!">
+    <meta name="keywords" content="corso di russo a bologna,
+    corso di italiano a bologna,
+    corso russo,
+    corso italiano,
+    scuola di lingue,
+    corso di lingua russa,
+    corso di lingua italiana,
+    lezioni di russo a Bologna,
+    lezioni di italiano a Bologna,
+    bologna centro,
+    portico delle parole,
+    porticodelleparole,
+    porticodelleparole.it,
+    offerta,
+    prezzi bassi,
+    sconti,
+    portici,
+    corsi di PACCHETTI PROMOZIONALI a Bologna,
+    corsi di lingue a Bologna,
+    corsi di inglese a Bologna,
+    corsi di russo a Bologna,
+    corsi di Italiano per stranieri a Bologna,
+    ripetizioni di russo a Bologna,
+    viaggi a Bologna,
+    giochi a Bologna,
+    test a Bologna,
+    ripetizioni di Russo a Bologna,
+    preparazione agli esami di maturità a Bologna,
+    preparazione esami universitari a Bologna,
+    ">
+
+    <!-- Twitter card -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@BolognaRusso" />
+    <meta name="twitter:title" content="Corsi di Russo a Bologna" />
+    <meta name="twitter:description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, 
+    offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!" />
+    <meta name="twitter:image" content="http://www.porticodelleparole.it/img/bologna-moscow-rainbow-portico.png" />
+
+    <!--  -->
+    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+
+    <title>
+        Privacy Policy - Associazione Portico delle Parole
+    </title>
+
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+        _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+        _gaq.push(['_setAccount', 'UA-44617461-1']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+    <script type="text/javascript" src='https://code.jquery.com/jquery-3.2.1.min.js'></script>
+    <script type="text/javascript" src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.js'></script>
+</head>
+
+<body>
+<div class="main-container shadow">
+    <h1>Privacy Policy</h1>
+
+    <p>
+        La presente informativa viene resa in ossequio all'art. 13 del Regolamento 2016/679 (GDPR), ai sensi dell'art. 13 del d.lgs. n. 196/2003 (Codice in materia di protezione dei dati personali) agli utenti che accedono al sito web <a href="www.porticodelleparole.it">www.porticodelleparole.it</a> ed è relativa a tutti i dati personali trattati secondo le modalità di seguito indicate.        
+    </p>
+
+    <h2>Il titolare del trattamento</h2>
+    <p>
+        Il Titolare del trattamento dei dati personali raccolti è il presidente della associazione <q>Portico delle Parole</q> Dmitry Myadzelets. Il Titolare pou essere contatato inviando apposita richiesta all’indirizzo email <a href="mailto:info@porticodelleparole.it">info@porticodelleparole.it</a>.
+    </p>
+
+    <h2>Dati racolti</h2>
+    <p>
+        I dati personali trattati sono raccolti in quanto forniti direttamente dall’interessato o forniti automaticamente.        
+    </p>
+    <p>
+        I dati forniti sono tutti i dati personali che siano forniti al Titolare del Trattamento con qualsiasi modalità, direttamente dall'interessato. In particolare sono raccolti e trattati i seguenti dati personali: nome, cognome, email, numero di telefono, livello di lingua russa.
+    </p>
+    <p>
+        I dati raccolti automaticamente sono i dati di navigazione. Il fornitore del servizio è <q>Google Analytics</q>, che ci permette di capire come i visitatori interagiscono con il sito, per permettere di ottimizzarlo e migliorarne le prestazioni. Tali dati, pur non essendo raccolti al fine di essere associati all’identità dell’utente, potrebbero indirettamente, mediante elaborazione e associazioni con dati raccolti dal Titolare, consentire la sua identificazione.
+    </p>
+
+    <h2>Finalità del trattamento</h2>
+    <p>
+        I dati sono trattati esclusivamente per le finalità per le quali sono raccolti, come di seguito descritte:
+        <ul>
+            <li>La corrispondenza e rintracciabilità dei soci alle l'attivita della associazione.</li>
+            <li>Il pagamento della quota associativa e di eventuali liberalità.</li>
+            <li>L’invio del periodico dell’associazione e di eventuale altro materiale informativo.</li>
+            <li>L’invio di informative via sms e altri sistemi di messaggistica (p.es. <q>WhatsApp</q>).</li>
+        </ul>
+    </p>
+        
+
+    <h2>Modalita del trattamento</h2>
+    <p>
+        I dati personali sono trattati con strumenti automatizzati per il tempo strettamente necessario a conseguire gli scopi per cui sono stati raccolti, prevedendo in ogni caso la verifica annuale dei dati conservati al fine di cancellare quelli ritenuti obsoleti, salvo che la legge non preveda obblighi di archiviazione.    
+    </p>
+    <p>
+        Il trattamento dei dati avviene di norma presso la sede della associazione, da personale o collaboratori esterni debitamente designati quali responsabili del trattamento. L’elenco completo dei responsabili del trattamento e degli Incaricati al trattamento dei dati personali può essere richiesto inviando apposita richiesta all’indirizzo email <a href="mailto:info@porticodelleparole.it">info@porticodelleparole.it</a>.
+    </p>
+    <p>
+        I dati non saranno comunicati a terzi, né saranno diffusi.        
+    </p>
+
+    <h2>Diritti dell'interessato</h2>
+    <p>
+        Ai sensi dell’art. 7 del Codice della Privacy e dell'art. 13 GDPR ciascun utente ha diritto di ottenere la conferma dell’esistenza o meno dei suoi dati personali, anche se non ancora registrati, e la loro comunicazione in forma intellegibile.        
+    </p>
+    <p>
+        In particolare, l’interessato ha diritto di ottenere da Titolare del trattamento l’indicazione:
+        <ul>
+            <li>dell’origine dei dati personali;</li>
+            <li>della finalità e delle modalità di trattamento;</li>
+            <li>gli estremi identificativi del titolare, dei responsabili e del rappresentante designato;</li>
+            <li>dei soggetti e delle categorie di soggetti ai quali i dati possono esserecomunicati o che possono venirne a conoscenza in qualità di rappresentante designato nel territorio dello Stato, di responsabili o incaricati.</li>
+        </ul>
+    </p>
+    <p>
+        Inoltre l’interessato ha diritto di ottenere da Titolare del trattamento:
+        <ul>
+            <li>l’aggiornamento, la rettificazione ovvero l’integrazione dei propri dati;</li>
+            <li>la trasformazione in forma anonima o il blocco dei dati trattati in violazione di legge;</li>
+            <li>l'accesso ai propri dati e cioè la conferma che sia o meno in corso un trattamento didati personali che lo riguardano;</li>
+            <li>la limitazione del trattamento;</li>
+            <li>la portabilità del dato, cioè il diritto di ricevere in formato strutturato i dati personali che lo riguardano.</li>
+        </ul>        
+    </p>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts-bottom.php" ?>
+</body>
+</html>
+
