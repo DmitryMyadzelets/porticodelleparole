c66d80cd970dae2dc058a12437f10561523856aa
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2019-09-26 19:33:30 +0200

    Remove debugging info

--- reviews/index.php
+++ reviews/index.php
@@ -1,7 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// Debugging in Browser
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 ?>
 
 <!DOCTYPE HTML>
