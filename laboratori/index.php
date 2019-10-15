1563c9f8f93f893aabca5f6e1d9c5c8dd4b7d8ef
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2019-10-15 11:32:08 +0200

    Add new page Laboratori

---
+++ laboratori/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Cache-control" content="private">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="description" content="Vuoi imparare il russo parlato e scritto in maniera coinvolgente, divertente e sempre motivante? Vieni da noi e fai il Corso di Lingua Russa a Bologna in Associazione Portoco delle Parole"> -->
+    <meta name="description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, 
+    offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!">
+    <meta name="keywords" content="
+    corsi di russo speciali,
+    corso di russo per adozione,
+    corso di russo per preparazione esami universitari,
+    corso di russo per azienda,
+    corso di russo per viaggiare
+    ">
+
+    <!-- Twitter card -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@BolognaRusso" />
+    <meta name="twitter:title" content="Corsi di Russo a Bologna" />
+    <meta name="twitter:description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, 
+    offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!" />
+    <meta name="twitter:image" content="http://www.porticodelleparole.it/img/bologna-moscow-rainbow-portico.png" />
+
+    <!--  -->
+    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+
+    <title>
+        Corsi di Russo Speciali a Bologna &mdash; Associazione Portico delle Parole
+    </title>
+
+    <?php include $_SERVER['DOCUMENT_ROOT']."/google-analytics.php" ?>
+
+</head>
+
+<body>
+<div class="main-container shadow">
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/logo.php" ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts.php" ?>
+
+<section>
+    <h1>
+        Laboratori linguistici
+    </h1>
+    <p>
+        Oltre alle lezioni del corso, avrai l'opportunità di migliorare il tuo russo con laboratori linguistici, un mezzo stimolante ed utile per ampliare le tue conoscenze.
+    </p>
+    <p>
+        I laboratori che proponiamo ti permetteranno di approfondire meglio vari aspetti della lingua, indicati per arricchire il tuo bagaglio lessicale, fornendo un approccio allo studio della lingua pratico e diretto. La pratica della lingua è stimolata con intelligenza e senso della realtà. Tra i vari laboratori da noi offerti/proposti, c'è il:
+    </p>
+    <ul>
+        <li><b>Laboratorio del lessico</b> (ad esempio, laboratorio dei proverbi e modi di dire, aggettivi, dello slang, il lessico relativo ai viaggi, all'aeroporto, lessico dell'alcool, ecc.);</li>
+        <li><b>Laboratorio di corsivo</b> (lezioni per imparare a leggere e scrivere il cirillico corsivo);</li>
+        <li><b>Laboratorio di grammatica</b> (approfondimenti su particolarità grammaticali, eccezioni, uso dei casi, verbi di moto, ecc);</li>
+        <li><b>Laboratorio di lettura</b> (esercitazioni di lettura ad alta voce di racconti o articoli legati al mondo russo, scelti accuratamente a seconda del livello di competenza degli studenti).</li>
+    </ul>
+    <p>
+        I laboratori sono completamente <b>gratuiti per i nostri corsisti</b> e si tengono due o tre volte al mese, sempre in orario serale e nei giorni feriali. Potrai partecipare ai laboratori anche se non sei un corsista acquistando la <q>Laboratori Linguistici Card</q>, al costo di 45 euro (15 per incontro), che ti consentirà di partecipare a 3 laboratori nel corso dell'anno accademico.
+    </p>
+</section>
+
+<hr>
+
+<section>
+    <p>
+        Guarda <a href="/#i-nostri-insegnanti">i nostri insegnanti</a>, 
+        cosa <a href="/#dicono-di-noi">dicono di noi</a> e
+        <a href="/#dove-trovarci">dove trovarci</a>.
+    </p>
+</section>
+
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts-bottom.php" ?>
+
+</body>
+</html>
