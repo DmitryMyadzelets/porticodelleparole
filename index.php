2de4e3e562f25bdb580b478ecf5094c744711718
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2021-02-26 13:27:38 +0100

    Add TRKI page

--- index.php
+++ index.php
@@ -75,7 +75,6 @@
 <?php include_once $_SERVER['DOCUMENT_ROOT']."/json/courses.json" ?>
 </script>
 
-
     <script>
         function autoResize(id){
             var newheight;
@@ -102,12 +101,13 @@
 <div class="main-container shadow">
 
 <ul class='menu'>
-    <li><a href="#corsi-di-russo">Corsi</a></li>
-    <li><a href="#prezzi">Prezzi</a></li>
-    <li><a href="#orari">Orari</a></li>
-    <li><a href="#attivita">Attività</a></li>
-    <li><a href="#i-nostri-insegnanti">Insegnanti</a></li>
-    <li><a href="#foto">Foto</a></li>
+    <li><a href="/#corsi-di-russo">Corsi</a></li>
+    <li><a href="/#prezzi">Prezzi</a></li>
+    <li><a href="/#orari">Orari</a></li>
+    <li><a href="/#attivita">Attività</a></li>
+    <li><a href="/#i-nostri-insegnanti">Insegnanti</a></li>
+    <li><a href="/#foto">Foto</a></li>
+    <li><a href="/trki/">TRKI</a></li>
     <li><a href="/test/">Test</a></li>
 </ul>
 
---
+++ trki/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!">
+    <meta name="keywords" content="
+TRKI,
+TORFL,
+Certificazione di lingua russa TRKI TORFL,
+Corsi di preparazione all'esame TRKI TORFL,
+Esami e certificazione TRKI TORFL
+    ">
+
+    <!-- Twitter card -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@BolognaRusso" />
+    <meta name="twitter:title" content="Corsi di Russo a Bologna" />
+    <meta name="twitter:description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!" />
+    <meta name="twitter:image" content="//www.porticodelleparole.it/img/bologna-moscow-rainbow-portico.png" />
+
+    <!--  -->
+    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+
+    <title>
+        Certificazione di lingua russa TRKI TORFL &mdash; Associazione Portico delle Parole
+    </title>
+
+    <?php include_once $_SERVER['DOCUMENT_ROOT']."/google-analytics.php" ?>
+
+</head>
+<body>
+<div class="main-container shadow">
+<?php include $_SERVER['DOCUMENT_ROOT']."/logo.php" ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts.php" ?>
+
+<h1>Certificazione di Lingua Russa TRKI</h1>
+
+<h2>Perchè sostenere l'esame di certificazione?</h2>
+<ul>
+  <li>Per essere più competitivo sul mercato del lavoro</li>
+  <li>Per andare a studiare in Russia o in un altro paese russofono</li>
+  <li>Per avere un riconoscimento ufficiale delle tue conoscenze linguistiche</li>
+</ul>
+<p>L'associazione <b>Portico delle Parole è un centro di certificazione ufficiale</b> della lingua russa TRKI/TORFL (Тест по русскому языку как иностранному/ Test of Russian as a foreign language), che ha stipulato un accordo con l'Università Statale di San Pietroburgo.</p>
+<p>La certificazione rilasciata viene riconosciuta dal MIUR (Ministero dell'Istruzione, dell'Università e della Ricerca).</p>
+<h2>Livelli e tariffe delle certificazioni</h2>
+<div class="div-table price-table">
+    <div class="div-column col-third">
+       <div class="col-head">
+           <h3>Livello</h3>
+       </div>
+       <p>A1 (elementare)</p>
+       <p>A2 (base)</p>
+       <p>B1 (intermedio)</p>
+       <p>B2 (avanzato)</p>
+       <p>C1 (padronanza)</p>
+       <p>C2 (padronanza)</p>
+     </div>
+    <div class="div-column col-third">
+      <div class="col-head">
+        <h3>Codice</h3>
+      </div>
+      <p>TEU</p>
+      <p>TBU</p>
+      <p>TRKI1</p>
+      <p>TRKI2</p>
+      <p>TRKI3</p>
+      <p>TRKI4</p>
+    </div>
+    <div class="div-column col-third">
+        <div class="col-head">
+            <h3>Prezzo</h3>
+        </div>
+        <p>77&#8364;</p>
+        <p>97&#8364;</p>
+        <p>107&#8364;</p>
+        <p>117&#8364;</p>
+        <p>127&#8364;</p>
+        <p>137&#8364;</p>
+    </div>
+</div>
+<p>Le certificazioni di livello A1 e A2 hanno validità di 5 anni, mentre le certificazioni di livello B1, B2, C1, C2 non hanno scadenza.</p>
+<h2>Quando si tengono gli esami?</h2>
+<p>Le sessioni d'esame dell'anno 2021 si terranno:<p>
+<ul>
+  <li>27&dash;28 maggio</li>
+  <li>14&dash;15 ottobre</li>
+</ul>
+<p>L’esame può essere svolto anche in una giornata.</p>
+<h2>Dove si tengono gli esami?</h2>
+<p>Tutte le sessioni d'esame si tengono presso le aule dell'associazione in Via del Pratello 9, 40122 Bologna (BO)</p>
+<h2>Le modalità d'esame</h2>
+<p>Per le certificazioni è consentito l'utilizzo:</p>
+<ul>
+  <li>Livelli A1, A2 e B1 &mdash; dizionario bilingue (RUS – IT,  IT – RUS).</li>
+  <li>Livelli B2 e C1 &mdash; unicamente dizionario monolingue (RUS – IT).</li>
+  <li>Livello C2 &mdash; non è ammesso l'utilizzo di nessun dizionario.</li>
+</ul>
+<p>Le sessioni d'esame sono strutturate in 5 prove:</p>
+<ol>
+  <li>Лексика и грамматика &mdash; conoscenze grammaticali e lessicali (prova scritta)</li>
+  <li>Чтение &mdash; abilità di comprensione scritta (prova scritta)</li>
+  <li>Письмо &mdash; abilità di produzione scritta (prova scritta)</li>
+  <li>Аудирование &mdash; abilità di comprensione orale (prova orale)</li>
+  <li>Говорение &mdash; abilità di produzione orale (prova orale)</li>
+</ol>
+<p>Per avere più informazioni sullo svolgimento degli esami puoi <a href="https://testingcenter.spbu.ru/vebinary.html" target="_blank">guardare i video</a> registrati dall'Università Statale di San Pietroburgo dalla durata di 1 ora per ogni livello. In lingua inglese per i livelli A1, B1, B2 ed in lingua russa per i livelli B2, C1, C2.</p>
+<p>Puoi <a href="https://testingcenter.spbu.ru/ekzameny/russia/trki.html" target="_blank">scaricare</a> le versioni demo del test per tutti i livelli.<p>
+<h2>Preparazione all'esame TRKI</h2>
+<p>Ti prepariamo a superare le varie prove di tutti i livelli dell'esame ufficiale TRKI.</p>
+<p>Ti forniamo i materiali adatti e ben organizzati che, insieme alle simulazioni fatte durante il corso e alla competenza dei nostri insegnanti, risulteranno necessari alla preparazione degli esami.</p>
+<p>Sono disponibili sia <a href="/#corsi-di-russo">corsi collettivi in piccoli gruppi che individuali</a>, <b>in sede</b> a Bologna oppure <b>online</b>. <a href="/#prezzi">Guarda le tariffe</a> dei nostri corsi.</p>
+
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts-bottom.php" ?>
+</body>
+</html>
