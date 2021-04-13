31603ea44ae8244f4fe0b9c1e92d0bb57f48d7c2
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2021-04-13 15:22:58 +0200

    Remove TRKI, chnage order, change text

--- corsi-speciali/index.php
+++ corsi-speciali/index.php
@@ -44,135 +44,76 @@
     Corsi di russo speciali
 </h1>
 
+<h2 id="russo-conversazione">
+    Corso di conversazione
+</h2>
+<div class='course-description'>
+    <img src="//www.porticodelleparole.it/photos/?id=5e4275f214888" alt="Conversazione">
+<p>Il mini-corso comprende i livelli dall' A1 al C1 ed è dedicato esclusivamente alla conversazione in lingua russa, quindi ti permetterà di acquisire maggior scioltezza nel parlato. Il programma prevede:</p>
+<ul>
+  <li>Discussioni su temi attuali che ti aiuteranno ad affrontare situazioni comunicative quotidiane e in ambito lavorativo.</li>
+  <li>Simulazioni di dialoghi in coppia in diversi contesti  (viaggi, acquisti, lavoro, università, relazioni personali, ecc) che ti faranno padroneggiare la lingua.</li>
+  <li>Riscontro immediato sui tuoi progressi da parte dei nostri insegnanti madrelingua.</li>
+</ul>
+<p>Il mini-corso di russo online oppure in sede a Bologna è strutturato in 4 lezioni da un'ora e mezza ciascuna. Il costo è di 12,80€ all'ora, per un costo complessivo di 77€ invece che di 100€! </p>
+</div>
+
 <h2 id="russo-per-azienda">
     Russo per l’azienda
 </h2>
 <div class='course-description'>
-    <img src="/img/business.jpg" alt="business">
-    <p>
-        Fin dalle prime lezione avrai modo apprendere e comprendere:
-    </p>
-    <ul>
-        <li>Le relazioni commerciali con la Federazione Russa e i paesi russofoni;</li>
-        <li>La terminologia aziendale;</li>
-        <li>La corrispondenza commerciale.</li>
-    </ul>
-    <p>
-        Conoscerai, così, le particolarità del business culture russa, le abitudini e la lingua degli interlocutori, 
-        così da poter comunicazione con partner e colleghi russofoni.
-    </p>
-    <p>
-        Il tutto è costantemente accompagnato da materiali chiari e ben organizzati, 
-        che potrai consultare facilmente anche autonomamente.
-    </p>
+<img src="/img/business.jpg" alt="business">
+<p>Durante il corso di russo online oppure in sede a Bologna conoscerai le abitudini e il linguaggio degli interlocutori russi in campo aziendale, e potrai metterli in pratica in situazioni comunicative con partner e colleghi russofoni.</p>
+<p>Potrai imparare:</p>
+<ul>
+  <li>Lessico aziendale;</li>
+  <li>Corrispondenza commerciale;</li>
+  <li>Relazioni commerciali con la Federazione Russa e i paesi russofoni;</li>
+  <li>Particolarità della business culture russa;</li>
+  <li>Simulazioni di trattative.</li>
+</ul>
+<p>Avrai a disposizione un materiale didattico completo e organizzato, che potrai consultare anche autonomamente.</p>
 </div>
 
 <h2 id="russo-per-preparazione-esami-universitari">
     Russo per preparazione esami universitari
 </h2>
 <div class='course-description'>
-    <img src="/img/russo-per-esami.jpg" alt="esami">
-    <p>
-        Questo corso ti permetterà di superare sia la parte scritta che quella orale degli 
-        esami universitari di lingua russa. Avrai la possibilità di avere:
-    </p>
-    <ul>
-        <li>Lezioni e materiali adatti per spiegare la grammatica russa in maniera chiara, logica e ben strutturata;</li>
-        <li>Conversazione pratica al fine di sviluppare le capacità comunicative dello studente.</li>
-    </ul>
-    <p>
-        Tutti gli studenti che hanno seguito i nostri corsi hanno superato con successo i loro esami. Ora tocca a te!
-    </p>
-</div>
-
-<h2 id="russo-per-adozione">
-    Russo per adozione
-</h2>
-<div class='course-description'>
-    <img src="/img/adozione.jpg" alt="adozione">
-    <p>
-        Programma speciale per genitori che vogliono adottare un bambino russo e che desiderano:
-    </p>
-    <ul>
-        <li>Imparare le nozioni basi della lingua russa;</li>
-        <li>Capire, conoscere meglio la personalità del bambino;</li>
-        <li>Comunicare con lui dai primi giorni.</li>
-    </ul>
-    <p>
-        Inoltre, il corso permette ai nuovi genitori di imparare come comportarsi durante il loro 
-        viaggio in Russia e come approcciarsi alla cultura russa nelle situazioni di vita 
-        quotidiana (ristoranti, negozi, indicazioni stradali ecc.).
-    </p>
-    <p>
-        Grazie all’esperienza acquisita negli anni, i materiali didattici sono pensati in modo tale 
-        da assimilare in tempi brevi tutte le capacità necessarie per questi obiettivi.
-    </p>
+<img src="/img/russo-per-esami.jpg" alt="esami">
+<p>Questo corso ti supporterà nella preparazione degli esami universitari di lingua russa, sia scritti sia orali.</p>
+<p>I nostri insegnanti ti spiegheranno la grammatica russa in maniera chiara, logica e ben strutturata, proponendoti un materiale didattico adatto al tuo esame.</p>
+<p>Ti aiuteremo anche a rafforzare la tua lingua parlata tramite attività di conversazione, e ti daremo un riscontro immediato sui tuoi progressi.</p>
+<p>Tutti gli studenti che hanno seguito i nostri corsi di russo online oppure in sede a Bologna hanno superato con successo i loro esami, speriamo di poter aiutare anche te.</p>
 </div>
 
 <h2 id="russo-per-viaggiare">
     Russo per viaggiare
 </h2>
 <div class='course-description'>
-    <img src="/img/turismo.jpg" alt="turismo">
-    <p>
-        Programma per imparare come usare la lingua russa nelle situazioni di viaggio più comuni.
-        Impara il vocabolario di russo per le tue vacanze per:
-    </p>
-    <ul>
-        <li>Andare a mangiare fuori;</li>
-        <li>Prenotare un hotel;</li>
-        <li>Chiedere informazioni sui prezzi;</li>
-        <li>Fare compere;</li>
-        <li>Viaggiare ed usare i mezzi pubblici ecc.</li>
-    </ul>
-    <p>
-        Per rendere il tuo viaggio più interessante e piacevole il corso si basa anche su 
-        informazioni utili riguardanti la cultura, la geografia e tanti consigli pratici su 
-        come affrontare la vita di tutti i giorni in Russia.
-    </p>
+<img src="/img/turismo.jpg" alt="turismo">
+<p>Durante il corso di russo online oppure in sede a Bologna potrai imparare l'alfabeto cirillico, un vocabolario utile, e mettere in pratica le tue nuove conoscenze in situazioni comunicative di viaggio come:</p>
+<ul>
+    <li>Andare a mangiare fuori;</li>
+    <li>Fare una prenotazione in un hotel;</li>
+    <li>Chiedere informazioni sui prezzi;</li>
+    <li>Fare compere;</li>
+    <li>Viaggiare ed usare i mezzi pubblici ecc.</li>
+</ul>
+<p>Per rendere il tuo viaggio più interessante e piacevole il corso comprende anche nozioni culturali e geografiche, e tanti consigli pratici su come affrontare la quotidianità in Russia.</p>
 </div>
 
-<h2 id="russo-per-trki">
-    Preparazione all'esame di certificazione internazionale ТРКИ-TORFL
+<h2 id="russo-per-adozione">
+    Russo per adozione
 </h2>
 <div class='course-description'>
-    <img src="//www.porticodelleparole.it/photos/?id=5e4271ddd6c4b" alt="ТРКИ-TORFL">
-    <p>
-        Ti prepariamo a superare delle prove di tutti i livelli dell'esame ufficiale TORFL (ТРКИ - тест по русскому языку как иностранному):
-    </p>
-    <ul>
-        <li><a href="http://www.torfl.it/TEU_Livello_elementare_-_A1">TEU Livello elementare – A1</a>;</li>
-        <li><a href="http://www.torfl.it/TBU_Livello_base_-_A2">TBU Livello base – A2</a>;</li>
-        <li><a href="http://www.torfl.it/TRKI1_-_B1">TRKI1 – B1</a>;</li>
-        <li><a href="http://www.torfl.it/TRKI2_-_B2">TRKI2 – B2</a>;</li>
-        <li><a href="http://www.torfl.it/TRKI3_-_C1">TRKI3 – C1</a>;</li>
-        <li><a href="http://www.torfl.it/TRKI4_-_C2">TRKI4 – C2</a>.</li>
-    </ul>
-    <p>
-        Avere una certificazione linguistica riconosciuta a livello internazionale può darti molti vantaggi, rendendoti più competitivo sul mercato del lavoro.
-    </p>
-</div>
-
-<h2 id="russo-conversazione">
-    Corso di conversazione
-</h2>
-<div class='course-description'>
-    <img src="//www.porticodelleparole.it/photos/?id=5e4275f214888" alt="Conversazione">
-    <p>
-        Il Portico delle Parole offre un corso dedicato esclusivamente alla conversazione in lingua russa!
-    </p>
-    <p>
-        Questo corso, con frequenza flessibile, ti permetterà di ripassare gli argomenti che hai già imparato, praticando concretamente la lingua e permettendoti di acquisire maggior scioltezza nel parlato.        
-    </p>
-    <p>
-        Durante il corso imparerai ad affrontare diverse situazioni comunicative nell’ambito del lavoro e della vita quotidiana, in viaggio o all’università, tramite simulazioni di situazioni tipiche e discussioni su temi attuali.
-    </p>
-    <p>
-        Durante le attività didattiche, da svolgere in gruppo o in coppia, sarai sempre seguito da insegnanti madrelingua che ti permetteranno di avere subito un riscontro sui tuoi progressi e ti aiuteranno a trovare il metodo di studio più adatto a te. 
-    </p>
-    <p>
-        Per iscriversi è necessario aver frequentato almeno 40 ore di un corso standard.
-    </p>
+<img src="/img/adozione.jpg" alt="adozione">
+<p>l corso ha lo scopo di supportare coloro che hanno intenzione di adottare un bambino in un paese russofono. Questo programma speciale vi permetterà di:</p>
+<ul>
+    <li>Imparare le nozioni basi della lingua russa.</li>
+    <li>Comprendere meglio la personalità del bambino e comunicare con lui fin dai primi giorni.</li>
+    <li>Capire come approcciarsi alla cultura russa in situazioni di vita quotidiana, e quindi come comportarsi durante il viaggio in Russia.</li>
+</ul>
+<p>Grazie all'esperienza acquisita negli anni, siamo riusciti a progettare un materiale didattico che permetta di raggiungere questi obiettivi in tempi brevi.</p>
 </div>
 
 <hr>
