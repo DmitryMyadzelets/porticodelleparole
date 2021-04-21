f590db049a83e904b0d156a7baef5536ee16b62e
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2021-04-21 14:04:27 +0200

    Move Russo Online to a separate page

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <ul class='menu'>
     <li><a href="/#corsi-di-russo">Corsi</a></li>
-    <li><a href="/#russo-online">Online</a></li>
+    <li><a href="/russo-online/#russo-online">Online</a></li>
     <li><a href="/#prezzi">Prezzi</a></li>
     <li><a href="/#orari">Orari</a></li>
     <li><a href="/#i-nostri-insegnanti">Insegnanti</a></li>
---
+++ russo-online/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="description" content="Vuoi imparare il russo parlato e scritto in maniera coinvolgente, divertente e sempre motivante? Vieni da noi e fai il Corso di Lingua Russa a Bologna in Associazione Portoco delle Parole"> -->
+    <meta name="description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, 
+    offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!">
+    <meta name="keywords" content="
+russo online,
+corsi di russo online,
+lezioni di russo online
+">
+
+    <!-- Twitter card -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@BolognaRusso" />
+    <meta name="twitter:title" content="Corsi di Russo a Bologna" />
+    <meta name="twitter:description" content="Insegniamo il russo, parlato e scritto, utilizzando un percorso ben strutturato per tutti i livelli, 
+    offrendo semplicità, grande coinvolgimento&hellip;. E un pizzico di divertimento!" />
+    <meta name="twitter:image" content="//www.porticodelleparole.it/img/bologna-moscow-rainbow-portico.png" />
+
+    <!--  -->
+    <link rel="stylesheet" type="text/css" href="/css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="/css/main.css">
+
+    <title>
+        Russo Online &mdash; Associazione Portico delle Parole
+    </title>
+
+    <?php include $_SERVER['DOCUMENT_ROOT']."/google-analytics.php" ?>
+
+</head>
+
+<body>
+<div class="main-container shadow">
+<?php include $_SERVER['DOCUMENT_ROOT']."/menu.php" ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/logo.php" ?>
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts.php" ?>
+
+<h1 id="russo-online">Russo online</h1>
+<img src="/img/russo-online-insegnanti.jpg" alt="Insegnianti di russo online" loading="lazy">
+<p>Ti diamo la possibilità di imparare il russo online su Zoom con i nostri corsi collettivi di piccoli
+gruppi (3-6 persone max) oppure individuali. Ecco i benefici che offrono le lezioni di russo online:</p>
+<ul>
+<li>Puoi seguire da qualsiasi posto con una connessione Internet: da casa tua, dal tuo ufficio durante
+la pausa pranzo, in trasferta, dalla tua seconda casa in mare o in montagna.</li>
+<li>Ti permettono di risparmiare il tempo del viaggio e il costo dei mezzi pubblici.</li>
+<li>Facilitano la partecipazione di ospiti provenienti dalla Russia, i quali arricchiscono enormemente
+le lezioni con i loro interventi.</li>
+<li>Danno la possibilità di utilizzare una ricca varietà di materiale didattico multimediale e
+interattivo.</li>
+<li>Durante le lezioni puoi consultare tutto il materiale di cui disponi, come libri, dizionari,
+appunti...senza doverteli portare dietro per tutta la giornata.</li>
+</ul>
+<p>Ti abbiamo fatte venire voglia di provare le lezioni di russo online? Iscriviti alla lezione di prova gratuita.</p>
+
+<hr>
+<p> Guarda <a href="/#prezzi">i prezzi</a> e cosa <a href="/#dicono-di-noi">dicono di noi</a>.</p>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts-bottom.php" ?>
+
+</body>
+</html>
