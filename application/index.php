1020cc3e66e80a147c0e2a8a6712f716b06a902a
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2022-02-18 22:01:27 +0100

    Cleanup

--- application/index.php
+++ application/index.php
@@ -24,58 +24,6 @@
   <p id="loader" class="center">
     <?php include "oval.svg"?>
   </p>
-  <?php /*
-	<form method="POST" action="<?php $_PHP_SELF ?>">
-
-	<p>Nome, Cognome<br/>
-		<input type="text" name="name" style="width:100%" required autofocus>
-	</p>
-
-	<p>Data di nascita<br/>
-		<input type="date" name="birthday" required>
-	</p>
-
-	<p>Data di nascita<br/>
-    <fieldset style="display: inline-block; width:4rem; border:none;">
-      <legend style="font-size:small;">&nbsp;Giorno&nbsp;</legend>
-      <input type="text" pattern="[0-9]+" required style="width:100%;">
-    </fieldset>
-    <fieldset style="display: inline-block; width:8rem; border:none;">
-      <legend style="font-size:small;">&nbsp;Mese&nbsp;</legend>
-      <input type="text" required style="width:100%;">
-    </fieldset>
-    <input type="text" required style="width:4rem;">
-	</p>
-
-	<p>Indirizzo e-mail<br/>
-		<input type="email" name="email" style="width:100%" required>
-	</p>
-
-  <p>Numero di telefono<br/>
-		<input type="tel" name="phone">
-	</p>
-
-	<p>
-		<label>
-			<input type="checkbox" name="statute_accepted" value="yes" required>
-      Accetto <a href="/statuto" target="_blank">lo Statuto della associazaione</a>
-    </label>
-	</p>
-
-	<p>
-		<label>
-			<input type="checkbox" name="gdpr_autorized" value="yes" required>
-			Autorizzo il trattamento dei dati personali in base all’art. 13 del D. Lgs. 196/2003 e all’art. 13 GDPR 679/16<br>
-		</label>
-	</p>
-
-  <p>
-    <input type="submit" value="Invia">
-  </p>
-
-  </form>
-  */?>
-
 </div>
 
 <?php include $_SERVER['DOCUMENT_ROOT']."/contacts-bottom.php" ?>
