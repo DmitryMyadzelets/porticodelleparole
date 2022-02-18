e9dc94c80fbb34bddbd56556fdcd167d26713ee1
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2022-02-18 19:21:06 +0100

    Initial commit

---
+++ applications/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html lang="it">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="robots" content="noindex">
+
+  <link async rel="stylesheet" type="text/css" href="/css/normalize.css">
+  <link async rel="stylesheet" type="text/css" href="/css/main.css">
+  <link async rel="stylesheet" type="text/css" href="//api.porticodelleparole.it/app.css">
+
+  <script defer src='//api.porticodelleparole.it/app.js'></script>
+
+  <title>Applications - Associazione Portico delle Parole</title>
+</head>
+
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT']."/header.php" ?>
+
+<div class="main-container shadow" id="app"></div>
+
+<?php include $_SERVER['DOCUMENT_ROOT']."/contacts-bottom.php" ?>
+</body>
+</html>
