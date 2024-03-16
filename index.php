ecabbc7b24366f295840a4011c030bd62bcf3648
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2024-03-16 15:06:45 +0100

    Move prices to a separate file

--- index.php
+++ index.php
@@ -404,78 +404,7 @@
 <div class="main-container shadow">
 
 <h2 id="prezzi">Prezzi</h2>
-
-<div class="div-table price-table">
-    <div class="div-column col-third">
-           <div class="col-head">
-               <h3>In gruppo</h3>
-           </div>
-           <p>
-               20 ore – <span style="text-decoration: line-through;">320&#8364;</span>
-               <span style="color:red"> adesso in offerta 287&#8364; (14,35 all'ora)</span>
-           </p>
-           <p>
-               I gruppi sono formati da 3 &mdash; max. 8 persone.
-           </p>
-           <p>
-               Durante il corso sono previste 3 lezioni di recupero.
-           </p>
-           <p>
-               Per i corsi collettivi offriamo uno sconto del 10&#37; per chi ha meno di 26 anni.
-           </p>
-     </div>
-    <div class="div-column col-third">
-        <div class="col-head">
-            <h3>In due</h3>
-        </div>
-        <p>
-            20 ore – 314&#8364; (15,70 all'ora)
-            <br/>
-            10 ore – 167&#8364; (16,70 all'ora)
-        </p>
-        <p>
-            Nei giorni feriali dopo le 17:30 e il sabato a qualsiasi orario:
-            <br/>
-            20 ore – 334&#8364; (16,70 all'ora)
-            <br/>
-            10 ore – 187&#8364; (18,70 all'ora)
-        </p>
-        <p>
-            Prezzo a persona
-        </p>
-    </div>
-    <div class="div-column col-third">
-       <div class="col-head">
-            <h3>Individuale</h3>
-        </div>
-        <p>
-            20 ore –
-            <span style="text-decoration: line-through;">517&#8364;</span>
-            <span style="color:red">
-              adesso in offerta 474&#8364; (23,70 all'ora)
-            </span>
-
-            <br/>
-
-            10 ore – 
-            <span style="text-decoration: line-through;">317&#8364;</span>
-            <span style="color:red">
-              adesso in offerta 257&#8364; (25,70 all'ora)
-            </span>
-        </p>
-        <p>
-            Nei giorni feriali dopo le 17:30 e il sabato a qualsiasi orario:
-            <br/>
-            20 ore – 534&#8364; (27,70 all'ora)
-            <br/>
-            10 ore – 287&#8364; (28,70 all'ora)
-        </p>
-        <p>
-            In sede oppure su Zoom
-        </p>
-    </div>
-</div>
-
+<?php include $_SERVER['DOCUMENT_ROOT']."/prices.php" ?>
 <p>
     È obbligatorio il rilascio di una tessera associativa del costo di 40&#8364;.
 </p>
