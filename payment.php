12a307b36d19131027e3ef73790f4e312e99e030
Dmitry Myadzelets <dmitrymyadzelets@gmail.com>
2024-03-16 16:01:34 +0100

    Show payment data as a table

--- payment.php
+++ payment.php
@@ -1,9 +1,15 @@
 <p>
   I metodi di pagamento: contanti, bonifico.
 </p>
-<p>
-  Dati per effettuare il bonifico:<br>
-  Intestatario: Portico delle Parole<br>
-  IBAN: IT27M0200802480000103405231<br>
-  BIC: UNCRITM1OM0
-</p>
+<div class="div-table price-table">
+  <div class="div-column">
+    <div class="col-head">
+      <h3>Dati per effettuare il bonifico</h3>
+    </div>
+    <p>
+      Intestatario: Portico delle Parole<br>
+      IBAN: IT27M0200802480000103405231<br>
+      BIC: UNCRITM1OM0
+    </p>
+  </div>
+</div>
